84aa4b90ce8587dd9640c801257abd8ff3f655a9
Mark Galloway <mgallowa@ualberta.ca>
2019-10-21 06:28:43 -0600

    Updates

---
+++ precache-manifest.083e819a5abfc0fee487e2d30b5c038f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf36a2fc07a82623e7c188c529fba57",
+    "url": "/IronInvoice/index.html"
+  },
+  {
+    "revision": "973e07b46d54b187855b",
+    "url": "/IronInvoice/static/css/main.8b415eba.chunk.css"
+  },
+  {
+    "revision": "a7a6c00cc8bd89f84b5f",
+    "url": "/IronInvoice/static/js/2.8d593ef2.chunk.js"
+  },
+  {
+    "revision": "973e07b46d54b187855b",
+    "url": "/IronInvoice/static/js/main.9e6c3957.chunk.js"
+  },
+  {
+    "revision": "8b9e342bd08e80a79f31",
+    "url": "/IronInvoice/static/js/runtime-main.407333cc.js"
+  }
+]);
